6a328bc6928f90eed7a6e019e96a45d543a04e41
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-04-06 14:28:07 -0300

    Great behaviour

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import {
 import Modal from 'react-native-modal';
 import {
   SafeAreaProvider,
+  SafeAreaView,
   useSafeAreaFrame,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
@@ -29,39 +30,64 @@ function ModalTester() {
     keyboard: visible,
     OS: Platform.OS,
   };
-  console.log('debug:', debug);
+  // console.log('debug:', debug);
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
   };
 
   return (
-    <View style={{flex: 1}}>
+    <SafeAreaView style={{flex: 1}}>
       <Button title="Show modal" onPress={toggleModal} />
 
       <Modal
         isVisible={isModalVisible}
-        style={{margin: 0, backgroundColor: 'white'}}
+        style={{margin: 0}}
         coverScreen
         onBackButtonPress={() => setModalVisible(false)}
         onBackdropPress={() => setModalVisible(false)}
         avoidKeyboard>
-        <ScrollView style={{paddingTop: insets.top}} bounces={false}>
+        <ScrollView
+          contentContainerStyle={{
+            paddingTop: insets.top,
+            height: dimensions.height - frame.y,
+            backgroundColor: 'white',
+          }}
+          bounces={false}>
           <Text>top!</Text>
-          <View style={{height: dimensions.height / 2}} />
+          <View style={{flex: 1}} />
 
           <Button title="Hide modal" onPress={toggleModal} />
           <View style={{height: 20}} />
+          <Text>Input 1</Text>
           <TextInput
             multiline
-            style={{borderColor: 'black', borderWidth: 1, height: 100}}
+            style={{
+              flex: 1,
+              textAlignVertical: 'top',
+              borderColor: 'black',
+              borderWidth: 1,
+              height: 100,
+            }}
           />
           <View style={{flex: 1}} />
-          <Button title="Hide modal" onPress={toggleModal} />
+          <Text>Input 2</Text>
+          <TextInput
+            multiline
+            style={{
+              flex: 1,
+              textAlignVertical: 'top',
+              borderColor: 'black',
+              borderWidth: 1,
+              height: 100,
+            }}
+          />
           <View style={{height: 20}} />
+          <Button title="Noop button" onPress={() => {}} />
+          <Text>bottom!</Text>
         </ScrollView>
       </Modal>
-    </View>
+    </SafeAreaView>
   );
 }
 
