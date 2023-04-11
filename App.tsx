2f6421e588e4516bd90207a08ea6fcb0298967b6
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-04-11 14:00:42 -0300

    Bottom was wrong on android device

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import useKeyboard from '@rnhooks/keyboard';
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {
   Button,
   Dimensions,
+  Keyboard,
   ScrollView,
   Text,
   TextInput,
@@ -23,35 +24,69 @@ function ModalTester() {
   const dimensions = useWindowDimensions();
   const insets = useSafeAreaInsets();
   const frame = useSafeAreaFrame();
+  const maxDeviceFrame = useRef(frame.height);
+  const [visibleWillShow] = useKeyboard({
+    useWillHide: true,
+    useWillShow: true,
+  });
+  const [visible] = useKeyboard();
+  if (frame.height > maxDeviceFrame.current) {
+    maxDeviceFrame.current = frame.height;
+  }
   const screen = Dimensions.get('screen');
   const window = Dimensions.get('window');
-  const [visible] = useKeyboard();
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
   };
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Button title="Show modal" onPress={toggleModal} />
+    <>
+      <ScrollView bounces={false}>
+        <SafeAreaView>
+          <Button title="Show modal" onPress={toggleModal} />
 
-      <TextInput />
-      <Text>
-        {JSON.stringify(
-          {
-            dimensions,
-            frame,
-            insets,
-            window,
-            screen,
-            initialWindowMetrics,
-            visible,
-          },
-          null,
-          2,
-        )}
-      </Text>
-
+          <TextInput />
+          <Text>
+            {JSON.stringify(
+              {
+                maxDeviceFrame: maxDeviceFrame.current,
+                visible,
+                visibleWillShow,
+                metrics: Keyboard.metrics() ?? null,
+                dimensions,
+                frame,
+                insets,
+                window,
+                screen,
+                initialWindowMetrics,
+              },
+              null,
+              2,
+            )}
+          </Text>
+        </SafeAreaView>
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            right: 60,
+            backgroundColor: 'red',
+            height: maxDeviceFrame.current,
+            width: 10,
+          }}
+        />
+        <View
+          style={{
+            position: 'absolute',
+            top: 1,
+            right: 60,
+            backgroundColor: 'green',
+            height: maxDeviceFrame.current - 2,
+            width: 10,
+          }}
+        />
+      </ScrollView>
       <Modal
         isVisible={isModalVisible}
         style={{margin: 0}}
@@ -62,7 +97,7 @@ function ModalTester() {
         <ScrollView
           contentContainerStyle={{
             paddingTop: insets.top,
-            height: dimensions.height - frame.y,
+            height: maxDeviceFrame.current,
             backgroundColor: 'white',
           }}
           bounces={false}>
@@ -97,9 +132,30 @@ function ModalTester() {
           <View style={{height: 20}} />
           <Button title="Noop button" onPress={() => {}} />
           <Text>bottom!</Text>
+
+          <View
+            style={{
+              position: 'absolute',
+              top: 0,
+              right: 80,
+              backgroundColor: 'red',
+              height: maxDeviceFrame.current,
+              width: 10,
+            }}
+          />
+          <View
+            style={{
+              position: 'absolute',
+              top: 1,
+              right: 80,
+              backgroundColor: 'green',
+              height: maxDeviceFrame.current - 2,
+              width: 10,
+            }}
+          />
         </ScrollView>
       </Modal>
-    </SafeAreaView>
+    </>
   );
 }
 
